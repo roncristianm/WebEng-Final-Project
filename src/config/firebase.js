6c06e37ed1eea09cd6cbc3c85581736e7c8b7432
roncristianm <roncristianm@gmail.com>
2026-02-08 17:28:10 +0800

    Firebase Configured
    - website is connected to firebase
    - included authentication method

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,23 +1,31 @@
-import { initializeApp } from 'firebase/app'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
-}
+  apiKey: "AIzaSyD-xy2-hiZpu8byq_7TB-E0T2haf1X2s88",
+  authDomain: "webeng-final-project-a2e02.firebaseapp.com",
+  projectId: "webeng-final-project-a2e02",
+  storageBucket: "webeng-final-project-a2e02.firebasestorage.app",
+  messagingSenderId: "365714681774",
+  appId: "1:365714681774:web:f2ac0f45f33303a433e23f",
+  measurementId: "G-4LRXF77X73"
+};
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase services
 export const auth = getAuth(app)
 export const db = getFirestore(app)
 export const storage = getStorage(app)
+export const analytics = getAnalytics(app)
 
 export default app
