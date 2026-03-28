523668f57cc4507d6d9686fb5f90acaa34d888e0
Exxoo <sanjuanjimuel029@gmail.com>
2026-03-28 16:25:59 +0800

    Fixing the security
    - removed any mention of the firebase API key
    - updated teh CI/Cd to include firebase measuremenbt

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,13 +5,13 @@ import { getStorage } from 'firebase/storage'
 import { getAnalytics } from 'firebase/analytics'
 
 const firebaseConfig = {
-  apiKey: "REDACTED",
-  authDomain: "nexxus-65975.firebaseapp.com",
-  projectId: "nexxus-65975",
-  storageBucket: "nexxus-65975.firebasestorage.app",
-  messagingSenderId: "421245750303",
-  appId: "1:421245750303:web:4b99e5f9877880112cd92a",
-  measurementId: "G-H60CE88R72"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 }
 
 // Initialize Firebase
